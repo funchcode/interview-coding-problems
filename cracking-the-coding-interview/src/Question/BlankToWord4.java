9ece8461056e6add233d137ebd38d25cb9386a40
정채환 <funchcode@gmail.com>
2019-03-13 17:09:52 +0900

    문제 : 공백을 다른 문자로 대체

--- cracking-the-coding-interview/src/Question/BlankToWord4.java
+++ cracking-the-coding-interview/src/Question/BlankToWord4.java
@@ -4,4 +4,18 @@ package Question;
 // 입력 예제 ) [문자열][충분한공백],[문자열길이]
 // 자바로 구현 시 문자 배열을 사용하여 필요한 연산을 각 문자에 바로 적용할 수 있도록 하라.
 public class BlankToWord4 {
+
+    public String blankToText(String str) {
+        return str.substring(0, getLength(str)).replaceAll(" ", "%20");
+    }
+
+    private int getLength(String str) {
+        return Integer.parseInt(str.split(",")[1].trim());
+    }
+
+    public static void main(String[] args) {
+        BlankToWord4 blankToWord = new BlankToWord4();
+        String input = "a bc de fg     , 10";
+        System.out.println(blankToWord.blankToText(input));
+    }
 }
