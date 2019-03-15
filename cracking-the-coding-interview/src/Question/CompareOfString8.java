8b4a2d17e0239c399ab5c7400328e4abee6681df
정채환 <funchcode@gmail.com>
2019-03-15 16:25:38 +0900

    미해결 문제

--- cracking-the-coding-interview/src/Question/CompareOfString8.java
+++ cracking-the-coding-interview/src/Question/CompareOfString8.java
@@ -3,4 +3,28 @@ package Question;
 // 한 단어가 다른 단어에 포함된 문자열인지 판별하는 isSubstring이라는 메서드가 있다고 가정한다.
 // s1과 s2의 두 문자열이 주어졌을 경우, s2가 s1을 회전시킨 결과인지 판별하는 코드를 isSubstring을 한 번만 호출하도록 하여 작성.
 public class CompareOfString8 {
+
+    public boolean isSubstring(String str1, String str2) {
+        boolean result = str1.matches(".*"+reverse(str2)+".*");
+
+        if(!result) {
+            result = str2.matches(".*"+reverse(str2)+".*");
+        }
+
+        return result;
+    }
+
+    private String reverse(String str) {
+        String temp = "";
+        for(int i = str.toCharArray().length-1 ; i >= 0 ; i--) {
+            temp = temp + str.charAt(i);
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        CompareOfString8 compareOfString = new CompareOfString8();
+        System.out.println(compareOfString.isSubstring("ABCDE", "ADCBA"));
+//        System.out.println("BCDDD".matches(".*BC.*"));
+    }
 }
--- cracking-the-coding-interview/src/Question/ComparePermutation3.java
+++ cracking-the-coding-interview/src/Question/ComparePermutation3.java
@@ -2,4 +2,17 @@ package Question;
 // 코딩 인터뷰 완벽 분석 *************
 // 문자열 두 개를 입력으로 받아 그중 하나가 다른 하나의 순열인지 판단하는 메서드를 작성하라.
 public class ComparePermutation3 {
+
+    public void permutation(String str1, String str2) {
+        char[] buffer = str1.toCharArray();
+
+
+    }
+
+    public static void main(String[] args) {
+        ComparePermutation3 comparePermutation = new ComparePermutation3();
+        String input1 = "abc";
+        String input2 = "bcabcabca";
+        comparePermutation.permutation(input1, input2);
+    }
 }
