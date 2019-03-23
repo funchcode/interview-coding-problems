b608a3a7a7216d71aeb798925127acec9958603f
정채환 <funchcode@gmail.com>
2019-03-23 12:11:26 +0900

    문제 Level1 : 완주하지 못한 마라톤 선수.v2

--- programmers/src/Level1/MarathonFinish.java
+++ programmers/src/Level1/MarathonFinish.java
@@ -44,4 +44,17 @@ public class MarathonFinish {
 
         return "";
     }
+
+    public String solutionV2(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        for (i = 0; i < completion.length ; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+
+        return participant[i];
+    }
 }
