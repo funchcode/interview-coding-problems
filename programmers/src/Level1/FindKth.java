672ddc4e6fe068cbb5c237a2c7b4df77867f5b14
정채환 <funchcode@gmail.com>
2019-03-24 15:27:08 +0900

    문제 Level1 : k번째 수

---
+++ programmers/src/Level1/FindKth.java
@@ -0,0 +1,34 @@
+package Level1;
+import java.util.*;
+
+// 문제 설명
+// : 배열 Array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구해라.
+// : 배열 Array와 i,j,k 2차원 배열을 매개변수로 받는다.
+
+// 제한 사항
+// : array의 길이는 1이상 100이하이다.
+// : array의 각 원소는 1이상 100이하이다.
+// : commands의 길이는 1이상 50이하이다.
+// : commands의 각 원소는 길이가 3입니다.
+
+// 입출력 예
+// 1 input : array = [1,5,2,6,3,7,4]    commands = [[2,5,3],[4,4,1],[1,7,3]]
+// 1 output : [5,6,3]
+public class FindKth {
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        List<Integer> temp;
+        for(int i = 0 ; i < commands.length ; i ++) {
+            temp = new ArrayList<>();
+            for(int j = commands[i][0]-1 ; j <= commands[i][1]-1 ; j ++) {
+                temp.add(array[j]);
+            }
+            Collections.sort(temp);
+            answer[i] = temp.get(commands[i][2]-1);
+        }
+
+        return answer;
+    }
+
+}
--- programmers/src/Main.java
+++ programmers/src/Main.java
@@ -1,14 +1,18 @@
+import Level1.FindKth;
 import Level1.MarathonFinish;
 import Level1.MockTest;
 import Level1.PrintRequest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        MockTest mockTest = new MockTest();
-        for(int i : mockTest.solution(new int[]{1,3,2,4,2})) {
+        FindKth findKth = new FindKth();
+        int [] result = findKth.solution(new int[]{1,5,2,6,3,7,4}, new int[][]{{2,5,3},{4,4,1},{1,7,3}});
+        for(int i : result) {
             System.out.println(i);
         }
     }
@@ -30,4 +34,8 @@ public class Main {
     for(int i : mockTest.solution(new int[]{1,3,2,4,2})) {
         System.out.println(i);
     }
+* */
+
+/*i번째부터 j번째 숫자까지 자르고 원하는 k번째 수 구하기
+*
 * */
