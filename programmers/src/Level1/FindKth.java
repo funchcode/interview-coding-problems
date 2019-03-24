042a86475fb1ec72dabaaecb066631d4b9553f41
정채환 <funchcode@gmail.com>
2019-03-24 15:37:54 +0900

    문제 Level1 : k번째 수.v2

--- programmers/src/Level1/FindKth.java
+++ programmers/src/Level1/FindKth.java
@@ -31,4 +31,16 @@ public class FindKth {
         return answer;
     }
 
+    public int[] version2(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i = 0 ; i < commands.length ; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2]-1];
+        }
+
+        return answer;
+    }
+
 }
--- programmers/src/Main.java
+++ programmers/src/Main.java
@@ -10,11 +10,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        FindKth findKth = new FindKth();
-        int [] result = findKth.solution(new int[]{1,5,2,6,3,7,4}, new int[][]{{2,5,3},{4,4,1},{1,7,3}});
-        for(int i : result) {
-            System.out.println(i);
-        }
+        
     }
 }
 
@@ -37,5 +33,9 @@ public class Main {
 * */
 
 /*i번째부터 j번째 숫자까지 자르고 원하는 k번째 수 구하기
-*
+    FindKth findKth = new FindKth();
+    int [] result = findKth.solution(new int[]{1,5,2,6,3,7,4}, new int[][]{{2,5,3},{4,4,1},{1,7,3}});
+    for(int i : result) {
+        System.out.println(i);
+    }
 * */
